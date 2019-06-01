0f5fee5e6e212081287df6f94131f3a608fee1f8
TanishaAgarwal <tanishaaggarwal2502@gmail.com>
2019-06-01 18:29:09 +0530

    delete file commited

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,24 @@
+const {MongoClient, ObjectID} = require('mongodb');
+ 
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if(err){
+        return console.log('unable to connect to mongo server');
+    }
+    console.log('connected to mongo server');
+    const db = client.db('TodoApp');
+
+    //deleteMany
+    //db.collection('Todos').deleteMany({text: 'eat lunch'}).then((result) => {
+       // console.log(result);
+    //});
+    //deleteOne
+    //db.collection('Todos').deleteOne({text: 'eat lunch'}).then((result) => {
+    //   console.log(result);
+   // });
+    //findOneAndDelete
+    db.collection('Users').findOneAndDelete(_id: new ObjectID("5cf178aea75ff42f61fde0cb")).then((result) => {
+        console.log(result);
+   });
+
+});
